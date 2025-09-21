9edc3063275d658822c7dcf173b00dc5a71f8a03
Ganesh Gupta <ganeshk.work@gmail.com>
2025-09-21 12:25:13 +0000

    align content to center (_for element section_)

--- src/components/sections/admissions.tsx
+++ src/components/sections/admissions.tsx
@@ -1,12 +1,11 @@
-import { admissionsData } from '@/lib/data';
 import AdmissionForm from '../client/admission-form';
 
 export default function Admissions() {
   return (
     <section id="admissions" className="py-16 md:py-24 bg-secondary/30">
-      <div className="container">
-        <div className="max-w-2xl mx-auto">
-            <AdmissionForm />
+      <div className="container flex justify-center">
+        <div className="max-w-2xl w-full">
+          <AdmissionForm />
         </div>
       </div>
     </section>
